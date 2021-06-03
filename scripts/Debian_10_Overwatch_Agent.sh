95db019e7211f46bcfe3e4ff7ea426226932b840
Ariel Paixão <apaixao@eusou.space>
2021-06-03 17:54:19 -0300

    Zabbix Agent

---
+++ scripts/Debian_10_Overwatch_Agent.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+echo "Type the Zabbix server IP: "
+read SERVER_IP
+
+echo "Hostname: $hostname"
+
+apt update -y
+apt upgrade -y
+
+apt install zabbix-agent -y
+
+cp /etc/zabbix/zabbix_agentd.conf /etc/zabbix/zabbix_agentd.conf.bkp
+
+echo "Server=$SERVER_IP" >> /etc/zabbix/zabbix_agentd.conf
+echo "ServerActive=$SERVER_IP" >> /etc/zabbix/zabbix_agentd.conf
+echo "Hostname=$hostname" >> /etc/zabbix/zabbix_agentd.conf
+
+systemctl restart zabbix-agent.service
+zabbix_agent -p
