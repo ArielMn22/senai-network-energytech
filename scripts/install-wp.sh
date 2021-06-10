884d63af136236a107f9c97cb626373a787946f2
Ariel Paixão dos Santos <arielpaixao10@gmail.com>
2021-06-09 23:31:40 -0300

    up

---
+++ scripts/install-wp.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+echo "Insert you IP Adress:"
+read ipaddress
+
+apt install -y apache2 php7.3 libapache2-mod-php7.3 php7.3-common php7.3-mbstring php7.3-xmlrpc php7.3-soap php7.3-gd php7.3-xml php7.3-intl php7.3-mysql php7.3-cli php7.3-ldap php7.3-zip php7.3-curl
+
+apt install -y default-mysql-server
+
+mysql_secure_installation
+
+echo "CREATE DATABASE wordpress;" | mysql -u root -p
+echo "GRANT ALL PRIVILEGES on wordpress.* TO 'wordpress_user'@'localhost' IDENTIFIED BY 'wordpress_pss123';" | mysql -u root -p
+echo "FLUSH PRIVILEGES;" | mysql -u root -p
+echo "EXIT;" | mysql -u root -p
+
+cd /tmp/
+wget -c https://wordpress.org/latest.tar.gz
+
+tar -xvzf latest.tar.gz
+mv wordpress/ /var/www/html/
+chown -R www-data:www-data /var/www/html/wordpress/
+chmod 755 -R /var/www/html/wordpress/
+
+echo "
+<VirtualHost *:80>
+     ServerAdmin admin@$ipaddress_error.com
+      DocumentRoot /var/www/html/wordpress
+     ServerName $ipaddress
+
+     <Directory /var/www/html/wordpress>
+          Options FollowSymlinks
+          AllowOverride All
+          Require all granted
+     </Directory>
+
+     ErrorLog ${APACHE_LOG_DIR}/$ipaddress_error.log
+     CustomLog ${APACHE_LOG_DIR}/$ipaddress_error_access.log combined
+
+</VirtualHost>" > /etc/apache2/sites-enabled/wordpress.conf
+
+ln -s /etc/apache2/sites-available/wordpress.conf /etc/apache2/sites-enabled/wordpress.conf
+a2enmod rewrite
+systemctl restart apache2
